8a1b087973b5291d826f2e2567e3534486a4c7e7
Nicetocupro <149050953+Nicetocupro@users.noreply.github.com>
2026-03-24 17:22:50 +0800

    Time: 0 ms (100%), Space: 23.3 MB (62.42%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        int left = 0;
+        int right = 0;
+
+        while(right < nums.size())
+        {
+            if(nums[right])
+            {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+                left++;
+            }
+            right++;
+        }
+    }
+};
+
+
+/*
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
         int count = 0;
         for(int j = 0; j < nums.size(); j++)
         {
@@ -18,3 +40,5 @@ public:
         }
     }
 };
+
+*/
