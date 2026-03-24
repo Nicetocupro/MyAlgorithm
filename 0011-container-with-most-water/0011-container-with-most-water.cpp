19002b649dd95c068589c7e75237de240354ab10
Nicetocupro <149050953+Nicetocupro@users.noreply.github.com>
2026-03-24 17:32:22 +0800

    Time: 2142 ms (5.19%), Space: 61.5 MB (55.9%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        // 越高越远越好
+        int left = 0;
+        int right = height.size() - 1;
+        int maxArea = 0;
+        while(left < right)
+        {
+            int tempArea = min(height[left], height[right]) * (right - left);
+            std::cout << height[left] << " " << height[right] << std::endl;
+            maxArea = max(maxArea, tempArea);
+            if(height[left] < height[right])
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+};
