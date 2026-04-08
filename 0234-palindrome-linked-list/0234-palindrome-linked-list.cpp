b1afa5dae4db971ce4a4141bc44741492622122e
Nicetocupro <149050953+Nicetocupro@users.noreply.github.com>
2026-04-08 12:23:28 +0800

    Time: 15 ms (13.97%), Space: 129.5 MB (5.13%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -13,15 +13,19 @@ public:
     bool isPalindrome(ListNode* head) {
         std::stack<ListNode*> myStack;
         ListNode* temp = head;
+        int nodeSum = 0;
 
         while(temp != nullptr)
         {
             myStack.push(temp);
             temp = temp->next;
+            nodeSum++;
         }
 
         temp = head;
-        while(temp != nullptr)
+        int step = 0;
+        // 只用校验一半 4 2 5 2
+        while(step != nodeSum / 2)
         {
             if(temp->val != myStack.top()->val)
             {
@@ -29,6 +33,7 @@ public:
             }
             temp = temp->next;
             myStack.pop();
+            step++;
         }
 
         return true;
