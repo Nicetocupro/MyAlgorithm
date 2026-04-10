6bca163d289737d345df34b4d75c7dcf2921f939
Nicetocupro <149050953+Nicetocupro@users.noreply.github.com>
2026-04-10 23:08:07 +0800

    Time: 11 ms (25.95%), Space: 10.8 MB (25.48%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -2,82 +2,35 @@ class Solution {
 public:
     string multiply(string num1, string num2) 
     {
-        if(num1.size() == 1 && num2.size() == 1)
+        if(num2 == "0" || num1 == "0")
         {
-            int str1 = num1[0] - '0';
-            int str2 = num2[0] - '0';
-
-            return to_string(str1 * str2);
-        }
-        else
-        {
-            pair<string , string> result1 , result2;
-            result1 = splitString(num1);
-            result2 = splitString(num2);
-            string ac = multiply(result1.first , result2.first);
-            string bd = multiply(result1.second , result2.second);
-            string ad = multiply(result1.first , result2.second);
-            string bc = multiply(result1.second , result2.first);
-
-            ac = ac + string(result1.second.size() + result2.second.size() , '0');
-            ad = ad + string(result1.second.size() , '0');
-            bc = bc + string(result2.second.size() , '0');
-
-            return removeLeadingZeros(addStrings(addStrings(ac, ad), addStrings(bc , bd)));
-        }
-    }
-
-    pair<string, string> splitString(const string& str) 
-    {
-        pair<string, string> result;
-        if(str.size() == 1)
-        {
-            result.first = "0";
-            result.second = str;
-        }
-        else
-        {
-            int length = str.length();
-            int mid = length / 2;
-
-            if (length % 2 == 0) {  // 如果字符串长度为偶数
-                result.first = str.substr(0, mid);
-                result.second = str.substr(mid);
-            } 
-            else    
-            {  // 如果字符串长度为奇数
-                result.first = str.substr(0, mid + 1);
-                result.second = str.substr(mid + 1);
-            }
+            return "0";
         }
 
-        return result;
-    }
-
-    string addStrings(const string& num1, const string& num2) {
-        int carry = 0;
         string result = "";
-        int i = num1.length() - 1, j = num2.length() - 1;
-    
-        // 从个位开始逐位相加
-        while (i >= 0 || j >= 0 || carry) {
-            int digit1 = (i >= 0) ? (num1[i--] - '0') : 0;
-            int digit2 = (j >= 0) ? (num2[j--] - '0') : 0;
-        
-            int sum = digit1 + digit2 + carry;
-            carry = sum / 10;
-            result = to_string(sum % 10) + result;
-        }
-        return result;
-    }
-    // 去除结果中的前导零
-    string removeLeadingZeros(const string& num) 
-    {
-        unsigned int index = 0;
-        while (index < num.length() - 1 && num[index] == '0') 
+        int step = 0;
+        int lastNum = 0;
+        while(step <= num1.size() + num2.size() - 2)
         {
-            index++;
+            int temp = 0;
+            for(int i = 0; i <= step; i++)
+            {
+                if(i < num1.size() && (step - i) < num2.size())
+                {
+                    temp += (num1[num1.size() - i - 1] - '0') * (num2[num2.size() - step + i - 1] - '0');
+                }
+            }
+            temp += lastNum;
+            lastNum = temp / 10;
+            temp = temp % 10;
+            result = std::to_string(temp) + result;
+            step++;
         }
-        return num.substr(index);
+        if(lastNum)
+        {
+            result = std::to_string(lastNum) + result;
+        }
+
+        return result;
     }
 };
