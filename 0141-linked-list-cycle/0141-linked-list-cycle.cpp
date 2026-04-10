77935662fb50eaf51c5e49303dd35a52be1bab2d
Nicetocupro <149050953+Nicetocupro@users.noreply.github.com>
2026-04-10 23:53:18 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.cpp
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -1,25 +1,9 @@
-class Solution {
-public:
-    bool hasCycle(ListNode *head) {
-        // 快慢指针 或者 set
-        if(head == NULL)
-        {
-            return false;
-        }
-
-        ListNode* slow = head;
-        ListNode* fast = head->next;
-
-        while(fast != NULL && fast->next != NULL)
-        {
-            if(slow == fast)
-            {
-                return true;
-            }
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-
-        return false;
+bool hasCycle(ListNode *head) {
+    ListNode *slow = head, *fast = head;
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) return true;
     }
-};
+    return false;
+}
